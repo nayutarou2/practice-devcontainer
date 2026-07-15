e86a3feb8655cc9b0df13135cd944798baa06cf6
akira <a.ono@infomart.co.jp>
2026-07-15 06:40:18 +0000

    initial commit

---
+++ src/main/java/com/example/practice_devcontainer/PracticeDevcontainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_devcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDevcontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDevcontainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_devcontainer/PracticeDevcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_devcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDevcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
