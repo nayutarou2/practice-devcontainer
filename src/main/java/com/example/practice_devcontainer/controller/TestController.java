e0bbeec1514728f58997f85d1cf72a6d20f4ff1d
akira <a.ono@infomart.co.jp>
2026-07-16 08:51:45 +0000

    test

--- src/main/java/com/example/practice_devcontainer/controller/TestController.java
+++ src/main/java/com/example/practice_devcontainer/controller/TestController.java
@@ -6,6 +6,8 @@ import com.example.practice_devcontainer.repository.TestRepository;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +19,7 @@ public class TestController {
     private final TestRepository testRepository;
 
     @GetMapping("/")
-    public ResponseEntity home() {
+    public ResponseEntity<?> home() {
         System.out.println("🔌 フロントからの疎通リクエストを検知しました！データを返します。");
 
         return ResponseEntity.ok().body(testRepository.selectAll());
